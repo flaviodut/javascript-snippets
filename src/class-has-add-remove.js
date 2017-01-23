6e5a77334e7538c242cd085a9ffe426ad84d206f
Flavio Dutra <flavio.dutra@hfocus.com.br>
2017-01-23 17:16:20 -0200

    Adicionado funções de verificação, adiciona e remover classes dos elementos

---
+++ src/class-has-add-remove.js
@@ -0,0 +1,29 @@
+// Funções de verificação, adicionar e remover classes do elemento
+// Source:
+// http://jaketrent.com/post/addremove-classes-raw-javascript/
+
+// Verifica se o elemento tem a classe
+// retorna true ou false
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+// Adiciona uma classe no elemento caso ele não tenha
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+// Remove classe específica do elemento caso ele tenha
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
