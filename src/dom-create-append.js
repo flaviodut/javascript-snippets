c41bf43cb5b83cb69a5e0cc77959c37927991370
Flavio Dutra <flavio.adore@gmail.com>
2017-01-30 23:17:37 -0200

    Renomeado funções

--- src/dom-create-append.js
+++ src/dom-create-append.js
@@ -2,10 +2,6 @@ function createElement(element) {
   return document.createElement(element);
 }
 
-function appendChild(element, parent) {
-  return document.querySelector(parent).appendChild(element);
+function appendElement(parent, element) {
+  return parent.appendChild(element);
 }
-
-// let para = createElement('p');
-// para.innerHTML = "ushauishai";
-// appendChild('body', para);
